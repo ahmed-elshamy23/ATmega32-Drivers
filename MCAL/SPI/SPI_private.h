a21b224e27594a05e3dddb49c9025cddc8fcd1be
Ahmed Elshamy <109276000+ahmed-elshamy23@users.noreply.github.com>
2023-01-25 10:03:14 +0200

    Update SPI_private.h

--- MCAL/SPI/SPI_private.h
+++ MCAL/SPI/SPI_private.h
@@ -3,9 +3,9 @@
 
 #include "STD_TYPES.h"
 
-#define SPCR (*(u8 *)(0x2D))
-#define SPSR (*(u8 *)(0x2E))
-#define SPDR (*(u8 *)(0x2F))
+#define SPCR (*(volatile u8 *)(0x2D))
+#define SPSR (*(volatile u8 *)(0x2E))
+#define SPDR (*(volatile u8 *)(0x2F))
 
 #define SCK_PIN 7
 #define MISO_PIN 6
